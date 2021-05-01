5cc5402f09996c735ec1a5dcd9f6a130092ec9f7
HariOm Dwivedi <dwivedihari987@gmail.com>
2021-05-01 21:54:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const dbRef = firebase.database().ref();
+const usersRef = dbRef.child('users');
+var config = {
+    apiKey: "cAIzaSyBnSrCl0UvzIq1yrDMJ3zsdummy",
+    authDomain: "asdfadsf-a56e7.firebaseapp.com",
+    databaseURL: "httpss://asdfadsf-a56e7.firebaseio.com",
+    projectId: "asdfadsf-a56e7",
+    storageBucket: "asdfadsf-a56e7.appspot.com",
+    messagingSenderId: "104313484945"
+  };
+  
+  firebase.initializeApp(config);
+
+  const userListUI = document.getElementById("userList");
+usersRef.on("child_added", snap => {
+    let user = snap.val();
+    let $li = document.createElement("li");
+    $li.innerHTML = user.name;
+    $li.setAttribute("child-key", snap.key);
+    $li.addEventListener("click", userClicked) userListUI.append($li);
+});
